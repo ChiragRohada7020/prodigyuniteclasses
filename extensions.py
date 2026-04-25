a02c5ec60f8f4ffcbacd9979dcc050f8b634c920
Chirag Rohada <chiragrohada40@gmail.com>
2026-04-26 00:51:54 +0530

    final clean version

--- extensions.py
+++ extensions.py
@@ -2,7 +2,9 @@ from pymongo import MongoClient
 from flask_mail import Mail
 
 # MongoDB
-client = MongoClient("mongodb://localhost:27017/ebook")
+client = MongoClient(
+    "mongodb+srv://chiragrohada40:Chirag%40123@cluster0.l9en3av.mongodb.net/flaskdb?retryWrites=true&w=majority"
+)
 db = client.get_default_database()
 
 # Mail (INIT only, no app yet)
